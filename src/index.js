7435825653b58f631aaa6d998c5239f50cb422b7
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-17 21:44:20 +0300

    23

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import './js/fetchCountries.js';
 import countryListTpl from './templates/countries.hbs';
 import countryCardTpl from './templates/country.hbs';
 import debounce from 'lodash.debounce';
+
+fetch('https://pixabay.com/api/?key=22969021-19f1494240440c9eacf690dfa&image_type=illustration&orientation=vertical')
+.then(response => response.json())
+.then(data => console.log(data.hits))
+.catch(err => console.log(err));
