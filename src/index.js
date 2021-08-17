293dd585eebcf8f8cc011dcc164f3cecc48b9845
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-17 14:09:17 +0300

    hm...

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('lol');
+import './css/styles.css';
+import './js/fetchCountries.js';
+import countryListTpl from './templates/countries.hbs';
+import countryCardTpl from './templates/country.hbs';
+import debounce from 'lodash.debounce';
