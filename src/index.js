61738192a922855c13cd5607c11dc46785efe15c
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-18 18:23:30 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -20,19 +20,42 @@ function onEnterName(e) {
 
   fCountries(inputValue, countryParams)
     .then(renderResultOfSearch)
-    .catch(err => console.log(err));
+    .catch(errResult);
 }
 
 function renderResultOfSearch(result) {
   if (result.length > 1 && result.length <= 10) {
     const markup = countryListTpl(result.map(item => item.name));
     refs.container.innerHTML = markup;
+    return;
   } else if (result.length === 1) {
     const markup = countryCardTpl(result[0]);
     refs.container.innerHTML = markup;
+    return;
+  } else if (result.length > 10) {
+    refs.container.innerHTML = '';
+    const errNote = error({
+      text: 'Too many countries found. Please enter a more specific query',
+      delay: 2000,
+      addClass: 'notify-err',
+      closer: false,
+      sticker: false,
+    });
+  } else if (result.status === 404) {
+    refs.container.innerHTML = '';
+    const errNotify = error({
+      text: 'No countries found. Please enter a more specific query',
+      delay: 2000,
+      addClass: 'notify-err',
+      closer: false,
+      sticker: false,
+    });
+  } else {
+    refs.container.innerHTML = '';
   }
 }
 
-
-
-
+function errResult(result) {
+  refs.container.innerHTML = '';
+  console.log('err', error);
+}
