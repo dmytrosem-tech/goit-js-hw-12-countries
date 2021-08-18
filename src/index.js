150f73b133803af1f45575cddedb1f9ede02a2d1
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-18 15:13:01 +0300

    almost

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import './css/styles.css';
-import './js/fetchCountries.js';
+import fCountries from './js/fetchCountries.js';
 import countryListTpl from './templates/countries.hbs';
 import countryCardTpl from './templates/country.hbs';
 import debounce from 'lodash.debounce';
+import { alert, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const countryParams = ['name', 'capital', 'population', 'languages', 'flag'];
+
+const refs = {
+  container: document.querySelector('.countries'),
+  inputField: document.querySelector('.input-field'),
+};
+
+refs.inputField.addEventListener('input', debounce(onEnterName, 1000));
+
+function onEnterName(e) {
+  const inputValue = e.target.value;
+
+  fCountries(inputValue, countryParams)
+    .then(renderResultOfSearch)
+    .catch(err => console.log(err));
+}
+// console.log('lol');
+
+function renderResultOfSearch(result) {
+  if (result.length > 1 && result.length <= 10) {
+    const markup = countryListTpl(result.map(item => item.name));
+    refs.container.innerHTML = markup;
+  } else if (result.length === 1) {
+    const markup = countryCardTpl(result[0]);
+    refs.container.innerHTML = markup;
+  }
+}
